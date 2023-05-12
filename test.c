b831e87fef21c8cb3540eb1db9aff6ad5534dc8b
fangpli <fangpli@ebay.com>
2023-05-12 14:04:56 +0800

    test mamory allocate

--- test.c
+++ test.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-int a ;
-int a;
-int b;
+#include <stdlib.h>
+#include <string.h>
 
-int b;
+#define MEM_SIZE 32 * 1024 * 1024 * 1024
 
-void f(){
-     printf("In file:%s, function:%s() and line:%d \n",
-           __FILE__, __func__, __LINE__);
-    printf("b = %d \n", b);
+int main() {
+    char* addr = (char*) malloc((long)MEM_SIZE);
+    printf("主线程调用malloc后, 目前共申请了 32gb 的虚拟内存\n");
+    
+    //循环频繁访问虚拟内存
+    while(1) {
+          printf("开始访问 32gb 大小的虚拟内存...\n");
+          memset(addr, 0, (long)MEM_SIZE);
+    }
+    return 0;
 }
