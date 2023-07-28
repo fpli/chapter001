8905b25a29e44df4f77e1bd869d5841799f2d481
fangpli <fangpli@ebay.com>
2023-07-28 09:47:17 +0800

    commit

--- test2.c
+++ test2.c
@@ -18,5 +18,16 @@ int main(int argc, char *argv[])
     char *pathvar;
     pathvar = getenv("PATH"); // getenv from stdlib.h
     printf("pathvar=%s",pathvar);
+
+
+    int a[] = {12, 30, 40, 50};
+    int *ptr = a;
+    printf("\n sizeof a: %lu", sizeof(a));
+    printf("\n sizeof ptr: %lu", sizeof(ptr));
+    char str1[500] = {'a', 'b'};
+    char * str2 = "hello world";
+    printf("\n sizeof str1: %lu", sizeof(str1));
+    printf("\n sizeof str2: %lu", sizeof(str2));
+    printf("\n");
     return 0;
 }
